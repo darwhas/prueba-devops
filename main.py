3687cb3090e3eaa3f69f47aa9d14eee47a996cd1
darwhas <darwhas-roso@hotmail.com>
2024-02-07 01:00:22 -0500

    devops: #comment applicaion python

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "prueba de devops"
+
+def sum(x, y):
+    return x + y
+
+
+def is_greater_than(number_1, number_2):
+    return number_1 > number_2
+
+
+def login(username, password):
+    if ((username == "uskokrum2010") and (password == "123456")):
+        return True
+    else:
+        return False
 
 app.run(host="0.0.0.0")
